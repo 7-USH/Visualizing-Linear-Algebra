72b713e71a5d689370df1f2cae608a6ae5b4556e
Tushar <tush1245@gmail.com>
2021-12-17 03:32:06 +0530

    all errors solved

--- ManimProjects/Eigen Faces/eigen_face.py
+++ ManimProjects/Eigen Faces/eigen_face.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from manimlib.imports import *
 from manim import *
+import cv
 
 
 class EigenFace(Scene):
@@ -12,26 +13,38 @@ class EigenFace(Scene):
         celebrity_photos = os.listdir(dir)[1:1001]
         celebrity_images = [dir+'/' + photo for photo in celebrity_photos]
         self.images = np.array([plt.imread(image)
-                           for image in celebrity_images], dtype=np.float64)
-        celebrity_names = [name[:name.find('0')-1].replace("_", " ")
-                           for name in celebrity_photos]
+                           for image in celebrity_images], dtype=np.float64)   
         n_samples, h, w = self.images.shape
-        self.plot_portraits(self.images, celebrity_names, h, w, n_row=4, n_col=4)
+        self.plot_portraits(self.images,h, w, n_row=4, n_col=4)
         self.clear()
 
-        P, C, M, Y = self.pca(n_samples,h,w, 1000)
+        X = self.images.reshape(n_samples, h*w)
+        P, C, M, Y = self.pca(X, 1000)
+
         eigenfaces = C.reshape((1000, h, w))
-        
-        bigGroup = Group()
-        for i in range(4 * 4):
-            window = ImageMobject(eigenfaces[i].reshape((h, w)))
-            window.scale(0.6)
-            bigGroup.add(window).arrange(direction=RIGHT)
-        bigGroup.arrange_in_grid(4, 4)
-        self.play(FadeIn(bigGroup))
+
+        plt.figure(figsize=(2.2 *4, 2.2 *4))
+        plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.20)
+        for i in range(4* 4):
+            plt.subplot(4, 4, i + 1)
+            plt.imshow(eigenfaces[i].reshape((h, w)), cmap=plt.cm.gray)
+            plt.xticks(())
+            plt.yticks(())
+        plt.savefig("output.png")
+    
+        meanImage = ImageMobject(M.reshape(h,w))
+        self.play(FadeIn(meanImage))
         self.wait()
-      
-    def plot_portraits(self, images, titles, h, w, n_row, n_col):
+        self.clear()
+
+        self.convertImage("output.png")
+
+        eigenFaceImages = ImageMobject("tush.png")
+        eigenFaceImages.scale(3)
+        self.play(FadeIn(eigenFaceImages))
+        self.wait()
+  
+    def plot_portraits(self, images,h, w, n_row, n_col):
         plt.figure(figsize=(2.2 * n_col, 2.2 * n_row))
         bigGroup = Group()
         plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.20)
@@ -43,11 +56,32 @@ class EigenFace(Scene):
         self.play(FadeIn(bigGroup))
         self.wait()    
          
-    def pca(self,n,h,w, n_pc):
-        X = self.images.reshape(n,h*w)
+    def pca(self,X, n_pc):
+        n_samples, n_features = X.shape
         mean = np.mean(X, axis=0)
         centered_data = X-mean
         U, S, V = np.linalg.svd(centered_data)
         components = V[:n_pc]
         projected = U[:, :n_pc]*S[:n_pc]
         return projected, components, mean, centered_data
+
+    def reconstruction(Y, C, M, h, w, image_index):
+        n_samples, n_features = Y.shape
+        weights = np.dot(Y, C.T)
+        centered_vector = np.dot(weights[image_index, :], C)
+        recovered_image = (M+centered_vector).reshape(h, w)
+        return recovered_image
+
+    def convertImage(self,image):
+        img = Image.open(image)
+        img = img.convert("RGBA")
+        datas = img.getdata()
+        newData = []
+        for items in datas:
+            if items[0] == 255 and items[1] == 255 and items[2] == 255:
+                newData.append((255, 255, 255, 0))
+            else:
+                newData.append(items)
+        img.putdata(newData)
+        img.save("./tush.png", "PNG")
+        print("Successful")
