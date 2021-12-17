c178cb11cdfebb015ccecc79e495797e97ed7a15
Tushar <tush1245@gmail.com>
2021-12-17 10:17:51 +0530

    eigen values recovered

--- ManimProjects/Eigen Faces/eigen_face.py
+++ ManimProjects/Eigen Faces/eigen_face.py
@@ -1,3 +1,4 @@
+from re import U
 from matplotlib import image
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,8 +16,18 @@ class EigenFace(Scene):
         self.images = np.array([plt.imread(image)
                            for image in celebrity_images], dtype=np.float64)   
         n_samples, h, w = self.images.shape
-        self.plot_portraits(self.images,h, w, n_row=4, n_col=4)
+        
+        resultTex = TextMobject(
+            "We have gathered some of the famous Celebrity images ")
+   
+        self.play(Write(resultTex))
+        self.wait()
         self.clear()
+
+
+        result = self.plot_portraits(self.images, h, w, n_row=4, n_col=4)
+        self.play(FadeIn(result))
+        self.wait(2)
 
         X = self.images.reshape(n_samples, h*w)
         P, C, M, Y = self.pca(X, 1000)
@@ -33,16 +44,43 @@ class EigenFace(Scene):
         plt.savefig("output.png")
     
         meanImage = ImageMobject(M.reshape(h,w))
-        self.play(FadeIn(meanImage))
+        groupy = Group()
+        groupy.add(meanImage)
+        self.play(Transform(result,groupy))
+        self.wait()
+
+        meanText = TextMobject("Mean of all the images")
+        meanText.next_to(groupy,direction=TOP)
+        self.play(Write(meanText))
+        self.wait()
+
+        self.clear()
+    
+        eigenFaceText = VGroup(
+            TextMobject("Using the mean image we are able"),
+            TextMobject("to achieve eigen Faces of all images")).arrange(direction=DOWN)
+        self.play(Write(eigenFaceText))
         self.wait()
         self.clear()
 
         self.convertImage("output.png")
-
-        eigenFaceImages = ImageMobject("tush.png")
-        eigenFaceImages.scale(3)
+        eigenFaceImages = ImageMobject("eigenFaces.png")
+        eigenFaceImages.scale(3)   
         self.play(FadeIn(eigenFaceImages))
+        self.wait(3)
+        self.clear()
+
+        recovered_images = [self.reconstruction(
+            Y, C, M, h, w, i) for i in range(len(self.images))]
+
+        recoverGroup = TextMobject("We can recover these Images")
+        self.play(Write(recoverGroup))
         self.wait()
+        self.clear()
+
+        recoveredResult = self.plot_portraits(recovered_images,h,w,4,4)
+        self.play(FadeIn(recoveredResult))
+        self.wait(2)    
   
     def plot_portraits(self, images,h, w, n_row, n_col):
         plt.figure(figsize=(2.2 * n_col, 2.2 * n_row))
@@ -53,8 +91,7 @@ class EigenFace(Scene):
             window.scale(0.6)
             bigGroup.add(window).arrange(direction=RIGHT)
         bigGroup.arrange_in_grid(4,4)         
-        self.play(FadeIn(bigGroup))
-        self.wait()    
+        return bigGroup    
          
     def pca(self,X, n_pc):
         n_samples, n_features = X.shape
@@ -65,8 +102,7 @@ class EigenFace(Scene):
         projected = U[:, :n_pc]*S[:n_pc]
         return projected, components, mean, centered_data
 
-    def reconstruction(Y, C, M, h, w, image_index):
-        n_samples, n_features = Y.shape
+    def reconstruction(self,Y, C, M, h, w, image_index):
         weights = np.dot(Y, C.T)
         centered_vector = np.dot(weights[image_index, :], C)
         recovered_image = (M+centered_vector).reshape(h, w)
@@ -83,5 +119,7 @@ class EigenFace(Scene):
             else:
                 newData.append(items)
         img.putdata(newData)
-        img.save("./tush.png", "PNG")
+        img.save("./eigenFaces.png", "PNG")
         print("Successful")
+
+    
